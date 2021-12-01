8cdcf3a2e92629475656d382171553c71ade0a74
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2021-12-01 02:43:55 +0100

    feat: flex added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import tw from 'twin.macro'
 
 const App = () => (
-  <div tw='flex justify-center items-center h-screen'>
+  <div tw='flex justify-center items-center h-screen flex-col'>
     <h1>Runway 💬</h1>
     <h1>Oya send money 😒😒😒 </h1>
   </div>
