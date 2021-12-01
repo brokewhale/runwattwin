a6d56facc13cae9d8e2f853bf366cada72f95d47
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2021-12-01 02:59:08 +0100

    feat: font added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,8 +22,21 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang='en'>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Source+Code+Pro'
+            rel='stylesheet'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Playfair+Display'
+            rel='stylesheet'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Dancing+Script'
+            rel='stylesheet'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Source Code Pro', ...defaultTheme.fontFamily.sans],
+        fancy: ['Dancing Script'],
+        playfair: ['Playfair Display'],
+      },
       colors: {
         electric: '#db00ff',
         ribbon: '#0047ff',
